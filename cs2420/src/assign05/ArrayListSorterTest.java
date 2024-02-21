89c59579085c75c9032c822a686357fc77c4ccb1
kierangriffin <kierangriffin27@gmail.com>
2024-02-20 18:48:58 -0700

    new stuff

--- cs2420/src/assign05/ArrayListSorterTest.java
+++ cs2420/src/assign05/ArrayListSorterTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static assign05.ArrayListSorter.*;
